cb2a98d7ad7a34ff14ba6a99ca93f9a6c326f475
asif02-git <abdulasif0411@gmail.com>
2026-07-28 19:27:15 +0530

    initial commit

---
+++ identifiers/identifiers1.py
@@ -0,0 +1,23 @@
+# Variable
+age = 20
+
+# Constant-style name (by convention)
+MAX_VALUE = 100
+
+# Function name
+def greet():
+    return "Hello"
+
+# Class name
+class Student:
+    pass
+
+# Name using an underscore
+student_name = "Srinu"
+
+# Printing all of them
+print("Variable:", age)
+print("Constant-style name:", MAX_VALUE)
+print("Function name:", greet())
+print("Class name:", Student.__name__)
+print("Name using an underscore:", student_name)
